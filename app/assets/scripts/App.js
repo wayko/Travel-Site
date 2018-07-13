452fdc594e0627a3e10667d60552a439c39aacab
Wayko <programmers@dreamcpu.com>
2018-07-13 08:28:20 -0400

    pushing current system
    pushing current system

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,3 @@
-function Person(endUser, favColor)
-{
-	this.name = endUser;
-	this.color = favColor;
-	this.greet = function()
-	{
-		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.color + ".");
-	}
-}
+import MobileMenu from './modules/MobileMenu';
 
-var john = new Person("John Doe", "Blue");
-john.greet();
-
-var john1 = new Person("Jane Smith", "Green");
-john1.greet();
+var mobileMenu = new MobileMenu();
--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -8,12 +8,12 @@ mixins = require('postcss-mixins'),
 hexrgba = require('postcss-hexrgba');
 
 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
-	.on('error', function(errorInfo){
-		console.log(errorInfo.toString());
-		this.emit('end');
-	})
-	.pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
